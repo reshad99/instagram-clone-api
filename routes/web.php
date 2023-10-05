0419c53aec36cd3fdd597bc439af764c10f2d72a
Reshad Memmedov <resat619916@gmail.com>
2023-10-05 12:43:59 +0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(route('admin.dashboard'));
+    return 'Working';
 });
 
 Route::get('/t', [SendMessageController::class, 'index']);
