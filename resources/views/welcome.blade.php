091bc944de73891fb695f47c73f09c04a924657f
Reshad Memmedov <resat619916@gmail.com>
2023-12-05 19:31:56 +0400

    websocket

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,7 @@
 
 
 <script>
-    const connection = new WebSocket('ws://127.0.0.1:8080/');
+    const connection = new WebSocket('wss://127.0.0.1:8080/');
 
     connection.onopen = () => {
         console.log('connection established')
