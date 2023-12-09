7f81a33aa43d2f541ec2f010a9a2c23e6af7f3bd
Reshad Memmedov <resat619916@gmail.com>
2023-12-09 18:50:07 +0400

    messages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Helpers\CmsSidebar;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Gate::define('viewLogViewer', function ($user) {
+        LogViewer::auth(function ($request) {
+            // return true to allow viewing the Log Viewer.
             return true;
         });
-
         view()->composer('admin.inc.left_sidebar', function () {
             $this->generateCmsSidebar();
             view()->share('sidebarItems', CmsSidebar::getInstance()->getItems());
